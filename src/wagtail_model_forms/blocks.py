840282b03ab0a4612ab4058b5a6819bdbb4678c0
Guido Erdtsieck <guidoerdtsieck@hotmail.nl>
2023-12-28 15:11:18 +0100

    feat: streamfield for modelforms

--- src/wagtail_model_forms/blocks.py
+++ src/wagtail_model_forms/blocks.py
@@ -1,7 +1,196 @@
+from django.utils.translation import gettext_lazy as _
 from wagtail import blocks
+from wagtail.fields import StreamField
 from wagtail.snippets.blocks import SnippetChooserBlock
 
 from wagtail_model_forms.settings import FORM_MODEL
+
+
+class AbstractFormFieldBlock(blocks.StructBlock):
+    label = blocks.CharBlock(label=_("label"))
+    help_text = blocks.CharBlock(required=False)
+    required = blocks.BooleanBlock(default=True, required=False)
+
+    class Meta:
+        abstract = True
+
+
+class ChoiceBlock(blocks.StructBlock):
+    value = blocks.CharBlock()
+
+
+class PlaceholderMixin(blocks.StructBlock):
+    placeholder = blocks.CharBlock(required=False)
+
+
+class DefaultValueMixin(blocks.StructBlock):
+    default_value = blocks.CharBlock(
+        required=False,
+        help_text=_(
+            "Comma separated values, should be the value of a choice or the label of a field"
+        ),
+    )
+
+
+class ChoicesMixin(blocks.StructBlock):
+    choices = blocks.ListBlock(ChoiceBlock())
+
+
+class SingleLineTextFieldBlock(
+    PlaceholderMixin, DefaultValueMixin, AbstractFormFieldBlock
+):
+    class Meta:
+        icon = "pilcrow"
+
+
+class MultipleLineTextFieldBlock(
+    PlaceholderMixin, DefaultValueMixin, AbstractFormFieldBlock
+):
+    class Meta:
+        icon = "pilcrow"
+
+
+class EmailFieldBlock(PlaceholderMixin, DefaultValueMixin, AbstractFormFieldBlock):
+    class Meta:
+        icon = "mail"
+
+
+class URLFieldBlock(PlaceholderMixin, DefaultValueMixin, AbstractFormFieldBlock):
+    class Meta:
+        icon = "site"
+
+
+class NumberFieldBlock(PlaceholderMixin, DefaultValueMixin, AbstractFormFieldBlock):
+    class Meta:
+        icon = "plus-inverse"
+
+
+class DateFieldBlock(PlaceholderMixin, DefaultValueMixin, AbstractFormFieldBlock):
+    class Meta:
+        icon = "date"
+
+
+class DateTimeFieldBlock(PlaceholderMixin, DefaultValueMixin, AbstractFormFieldBlock):
+    class Meta:
+        icon = "time"
+
+
+class DropdownFieldBlock(DefaultValueMixin, ChoicesMixin, AbstractFormFieldBlock):
+    class Meta:
+        icon = "arrow-down"
+
+
+class RadioFieldBlock(DefaultValueMixin, ChoicesMixin, AbstractFormFieldBlock):
+    class Meta:
+        icon = "radio-full"
+
+
+class CheckboxFieldBlock(DefaultValueMixin, AbstractFormFieldBlock):
+    class Meta:
+        icon = "tick-inverse"
+
+
+class CheckboxesFieldBlock(DefaultValueMixin, ChoicesMixin, AbstractFormFieldBlock):
+    default_value = blocks.CharBlock(
+        required=False, help_text=_("Comma separated values")
+    )
+
+    class Meta:
+        icon = "tick-inverse"
+
+
+class FileFieldBlock(AbstractFormFieldBlock):
+    class Meta:
+        icon = "doc-empty-inverse"
+
+
+class HiddenFieldBlock(AbstractFormFieldBlock):
+    class Meta:
+        icon = "form"
+
+
+class MultipleSelectFieldBlock(DefaultValueMixin, ChoicesMixin, AbstractFormFieldBlock):
+    class Meta:
+        icon = "list-ul"
+
+
+# Do not change the given names(strings) of the fieldblocks, these are used for method lookups.
+TEXT_INPUT_FIELDBLOCKS = [
+    ("singleline", SingleLineTextFieldBlock()),
+    ("multiline", MultipleLineTextFieldBlock()),
+    ("email", EmailFieldBlock()),
+    ("url", URLFieldBlock()),
+    ("number", NumberFieldBlock()),
+    ("date", DateFieldBlock()),
+    ("datetime", DateTimeFieldBlock()),
+]
+
+CHOICE_FIELDBLOCKS = [
+    ("dropdown", DropdownFieldBlock()),
+    ("radio", RadioFieldBlock()),
+    ("checkbox", CheckboxFieldBlock()),
+    ("checkboxes", CheckboxesFieldBlock()),
+]
+
+UTILITY_FIELDBLOCKS = [
+    ("file", FileFieldBlock()),
+    ("hidden", HiddenFieldBlock()),
+    ("multiselect", MultipleSelectFieldBlock()),
+]
+
+COMMON_FIELDBLOCKS = TEXT_INPUT_FIELDBLOCKS + CHOICE_FIELDBLOCKS + UTILITY_FIELDBLOCKS
+
+
+class FieldRowBlock(blocks.StructBlock):
+    label = blocks.CharBlock(required=False)
+    fields = blocks.StreamBlock(
+        COMMON_FIELDBLOCKS,
+        icon="form",
+        use_json_field=True,
+    )
+
+    class Meta:
+        icon = "form"
+
+
+class FieldSetBlock(blocks.StructBlock):
+    legend = blocks.CharBlock(required=False)
+    label = blocks.CharBlock(required=False)
+    fields = blocks.StreamBlock(
+        COMMON_FIELDBLOCKS + [("fieldrow", FieldRowBlock())],
+        icon="form",
+        use_json_field=True,
+    )
+
+    class Meta:
+        icon = "list-ul"
+
+
+# Do not change the given names(strings) of the fieldblocks, these are used for method lookups.
+FIELDBLOCKS = StreamField(
+    [
+        ("singleline", SingleLineTextFieldBlock()),
+        ("multiline", MultipleLineTextFieldBlock()),
+        ("email", EmailFieldBlock()),
+        ("url", URLFieldBlock()),
+        ("number", NumberFieldBlock()),
+        ("date", DateFieldBlock()),
+        ("datetime", DateTimeFieldBlock()),
+        ("dropdown", DropdownFieldBlock()),
+        ("radio", RadioFieldBlock()),
+        ("checkbox", CheckboxFieldBlock()),
+        ("checkboxes", CheckboxesFieldBlock()),
+        ("file", FileFieldBlock()),
+        ("hidden", HiddenFieldBlock()),
+        ("multiselect", MultipleSelectFieldBlock()),
+        ("fieldset", FieldSetBlock()),
+        ("fieldrow", FieldRowBlock()),
+    ],
+    blank=True,
+    null=True,
+    verbose_name="Fields",
+    use_json_field=True,
+)
 
 
 class AbstractFormBlock(blocks.StructBlock):
--- src/wagtail_model_forms/models.py
+++ src/wagtail_model_forms/models.py
@@ -1,18 +1,20 @@
 import json
+from collections import OrderedDict
 
+from django import forms
+from django.conf import settings
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.utils.functional import cached_property
+from django.utils.html import conditional_escape
 from django.utils.translation import gettext_lazy as _
-from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
 from wagtail.admin.panels import (
     FieldPanel,
-    InlinePanel,
     ObjectList,
     TabbedInterface,
 )
-from wagtail.contrib.forms.forms import FormBuilder
+from wagtail.contrib.forms.forms import FormBuilder as BaseFormBuilder
 from wagtail.contrib.forms.models import (
     AbstractFormField as WagtailAbstractFormField,
 )
@@ -21,7 +23,7 @@ from wagtail.contrib.forms.models import (
 )
 
 from wagtail_model_forms import get_submission_model
-from wagtail_model_forms.settings import FORM_MODEL
+from wagtail_model_forms.blocks import FIELDBLOCKS
 
 
 class AbstractFormSubmission(WagtailAbstractFormSubmission):
@@ -45,6 +47,161 @@ class AbstractFormField(WagtailAbstractFormField):
         abstract = True
 
 
+class FormBuilder(BaseFormBuilder):
+    def create_singleline_field(self, field, options):
+        # TODO: This is a default value - it may need to be changed
+        options["max_length"] = 255
+        return forms.CharField(
+            widget=forms.TextInput(attrs={"placeholder": field["placeholder"]}),
+            **options
+        )
+
+    def create_multiline_field(self, field, options):
+        return forms.CharField(
+            widget=forms.Textarea(attrs={"placeholder": field["placeholder"]}),
+            **options
+        )
+
+    def create_date_field(self, field, options):
+        return forms.DateField(
+            widget=forms.DateInput(attrs={"placeholder": field["placeholder"]}),
+            **options
+        )
+
+    def create_datetime_field(self, field, options):
+        return forms.DateTimeField(
+            widget=forms.DateTimeInput(attrs={"placeholder": field["placeholder"]}),
+            **options
+        )
+
+    def create_email_field(self, field, options):
+        return forms.EmailField(
+            widget=forms.EmailInput(attrs={"placeholder": field["placeholder"]}),
+            **options
+        )
+
+    def create_url_field(self, field, options):
+        return forms.URLField(
+            widget=forms.URLInput(attrs={"placeholder": field["placeholder"]}),
+            **options
+        )
+
+    def create_number_field(self, field, options):
+        return forms.DecimalField(
+            widget=forms.NumberInput(attrs={"placeholder": field["placeholder"]}),
+            **options
+        )
+
+    def create_file_field(self, field, options):
+        return forms.FileField(**options)
+
+    def handle_fieldset(self, structvalue, formfields):
+        fieldset = structvalue.value
+        fieldset_legend = fieldset["legend"]
+        fieldset_label = fieldset["label"]
+        for structvalue in fieldset["fields"]:
+            field_type = str(structvalue.block_type)
+            if field_type == "fieldrow":
+                self.handle_fieldrow_in_fieldset(
+                    structvalue, formfields, fieldset_label, fieldset_legend
+                )
+            else:
+                self.handle_fieldset_field(
+                    structvalue, formfields, fieldset_legend, fieldset_label
+                )
+
+    def handle_fieldrow(self, structvalue, formfields):
+        fieldrow = structvalue.value
+        fieldrow_label = fieldrow["label"]
+        for structvalue in fieldrow["fields"]:
+            self.handle_fieldrow_field(structvalue, formfields, fieldrow_label)
+
+    def handle_normal_field(self, structvalue, formfields):
+        field = structvalue.value
+        options = self.get_field_options(field)
+        create_field = self.get_create_field_function(str(structvalue.block_type))
+        clean_name = field["label"]
+        formfields[clean_name] = create_field(field, options)
+
+    def handle_fieldset_field(
+        self, structvalue, formfields, fieldset_legend, fieldset_label
+    ):
+        field = structvalue.value
+        options = self.get_field_options(field)
+        create_field = self.get_create_field_function(str(structvalue.block_type))
+        clean_name = field["label"]
+        formfields[clean_name] = create_field(field, options)
+        formfields[clean_name].fieldset_legend = fieldset_legend
+        formfields[clean_name].fieldset_label = fieldset_label
+
+    def handle_fieldrow_field(self, structvalue, formfields, fieldrow_label):
+        field = structvalue.value
+        options = self.get_field_options(field)
+        create_field = self.get_create_field_function(str(structvalue.block_type))
+        clean_name = field["label"]
+        formfields[clean_name] = create_field(field, options)
+        formfields[clean_name].fieldrow_label = fieldrow_label
+
+    def handle_fieldrow_in_fieldset(
+        self, structvalue, formfields, fieldset_label, fieldset_legend
+    ):
+        fieldrow = structvalue.value
+        fieldrow_label = fieldrow["label"]
+        clean_name = fieldrow["label"]
+        for structvalue in fieldrow["fields"]:
+            self.handle_fieldrow_field(structvalue, formfields, fieldrow_label)
+            formfields[clean_name].fieldset_legend = fieldset_legend
+            formfields[clean_name].fieldset_label = fieldset_label
+
+    @property
+    def formfields(self):
+        formfields = OrderedDict()
+
+        for structvalue in self.fields:
+            field_type = str(structvalue.block_type)
+            if field_type == "fieldset":
+                self.handle_fieldset(structvalue, formfields)
+            elif field_type == "fieldrow":
+                self.handle_fieldrow(structvalue, formfields)
+            else:
+                self.handle_normal_field(structvalue, formfields)
+        return formfields
+
+    def get_field_options(self, field):
+        options = {"label": field["label"]}
+        if getattr(settings, "WAGTAILFORMS_HELP_TEXT_ALLOW_HTML", False):
+            options["help_text"] = field["help_text"]
+        else:
+            options["help_text"] = conditional_escape(field["help_text"])
+        options["required"] = field["required"]
+        if "default_value" in field:
+            options["initial"] = field["default_value"]
+        return options
+
+    def get_formatted_field_choices(self, field):
+        """
+        Returns a list of choices [(string, string),] for the field.
+        Split the provided choices into a list, separated by new lines.
+        If no new lines in the provided choices, split by commas.
+        """
+
+        choices = []
+        for choice in field["choices"]:
+            choices.append((choice["value"], choice["value"]))
+        return choices
+
+    def get_formatted_field_initial(self, field):
+        """
+        Returns a list of initial values [string,] for the field.
+        Split the supplied default values into a list, separated by new lines.
+        If no new lines in the provided default values, split by commas.
+        """
+        if "default_value" in field:
+            values = [x.strip() for x in field["default_value"].split(",")]
+
+        return values
+
+
 class AbstractForm(ClusterableModel):
     title = models.CharField(
         verbose_name=_("title"),
@@ -53,9 +210,11 @@ class AbstractForm(ClusterableModel):
 
     form_builder = FormBuilder
 
+    fields = FIELDBLOCKS
+
     form_panels = [
         FieldPanel("title", classname="full"),
-        InlinePanel("form_fields", label="Form fields"),
+        FieldPanel("fields"),
     ]
 
     edit_handler = TabbedInterface(
@@ -75,7 +234,8 @@ class AbstractForm(ClusterableModel):
         return None
 
     def get_form_fields(self):
-        return self.form_fields.all()
+        form_fields = self.fields
+        return form_fields
 
     def get_data_fields(self):
         data_fields = [
