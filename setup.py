66966eaece2379ee59e36c778051f7bd82a5a7bc
Rob Moorman <rob@moori.nl>
2024-03-11 15:50:23 +0100

    chore: version bump fix: namespace for fieldrow (in fieldset)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ tests_requires = [
 
 setup(
     name="wagtail-model-forms",
-    version="0.4.5",
+    version="0.4.6",
     description="",
     author="R. Moorman <rob@vicktor.nl>",
     install_requires=install_requires,
