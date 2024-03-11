ec11e7099d9e3ec129ab6d0ebb20988e91a14ce9
Storm B. Heg <storm@stormbase.digital>
2024-03-11 10:49:58 +0100

    Namespace form fields to make their HTML names more unique
    If you have two fieldsets, say Parent 1 and Parent 2, each with a field
    'Phone Number', the resulting phone number fields will have the same
    HTML name. This means the last field entry overrides all other values.
    
    This commit aims to fix this situation by including the containing
    fieldset/fieldrow name.
    
    There still will be issues if two fields on the toplevel are named the
    same.

--- src/wagtail_model_forms/contrib/crispy_forms.py
+++ src/wagtail_model_forms/contrib/crispy_forms.py
@@ -1,7 +1,9 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Div, Field, Fieldset, Layout, Row
+from django.utils.text import slugify
 
 from wagtail_model_forms.settings import CIRSPY_FORMS_FORM_TAG
+from wagtail_model_forms.models import get_field_clean_name
 
 
 class CrispyFormLayoutMixin:
@@ -23,21 +25,25 @@ class CrispyFormLayoutMixin:
         layout = Layout(*layout_objects)
         return layout
 
-    def get_layout_objects_from_field(self, field, css_class=None):
+    def get_layout_objects_from_field(self, field, css_class=None, namespace=""):
         layout_objects = []
         block_type = field.block_type
+
         if block_type == "fieldset":
+            namespace = slugify(field.value["legend"])
             child_objects = []
             for child_field in field.value["form_fields"]:
-                child_objects += self.get_layout_objects_from_field(child_field)
+                child_objects += self.get_layout_objects_from_field(child_field, namespace=namespace)
             layout_objects.append(Fieldset(field.value["legend"], *child_objects))
         elif block_type == "fieldrow":
+            namespace = slugify(field.value["legend"])
             child_objects = []
             for child_field in field.value["form_fields"]:
                 child_objects += self.get_layout_objects_from_field(
-                    child_field, css_class="col"
+                    child_field, css_class="col", namespace=namespace
                 )
             layout_objects.append(Row(*child_objects))
         else:
-            layout_objects.append(Div(Field(field.value["label"]), css_class=css_class))
+            clean_name = get_field_clean_name(field.value, namespace)
+            layout_objects.append(Div(Field(clean_name), css_class=css_class))
         return layout_objects
