ac5695b617500d3724e3dda98a7ae49616ccd63b
Rob Moorman <rob@moori.nl>
2024-07-01 20:22:28 +0200

    chore: version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ tests_requires = [
 
 setup(
     name="wagtail-model-forms",
-    version="0.4.7",
+    version="0.4.8",
     description="",
     author="R. Moorman <rob@vicktor.nl>",
     install_requires=install_requires,
